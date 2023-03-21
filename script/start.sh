7c153fb1bd828a03d1e77fd51938943a4355e626
swat9013 <12694496+swat9013@users.noreply.github.com>
2023-03-21 22:07:56 +0900

    Synology DS218上で動くようにscriptを修正

--- script/start.sh
+++ script/start.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")"/../
 
-docker build -t keep2todoist:latest ./app
-docker run -d --restart always -v "$(pwd)"/app/config.yaml:/app/config.yaml keep2todoist:latest
+docker build --network=host -t keep2todoist:latest ./app
+docker run --network=host -v /dev:/dev -d --restart always -v "$(pwd)"/app/config.yaml:/app/config.yaml keep2todoist:latest
